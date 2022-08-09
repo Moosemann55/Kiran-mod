8f6be03875c5dff26e692b106c00046f37c9413e
Moosemann55 <Moosemann55@gmail.com>
2022-08-09 17:21:23 -0400

    added very important comment

--- src/main/java/net/kiran/kiranmod/KiranMod.java
+++ src/main/java/net/kiran/kiranmod/KiranMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//very important comment
 public class KiranMod implements ModInitializer {
 	public static final String MOD_ID = "kiranmod";
 	// This logger is used to write text to the console and the log file.
